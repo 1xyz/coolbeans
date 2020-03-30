6ed5d93a0aaa1f79b8e9ea0f1bbb81ef77ff0bb8
1xyz <58613330+1xyz@users.noreply.github.com>
2020-03-30 16:00:05 -0700

    Skeleton code for store, raft node configuration
    Summary: Added skeleton code for main control, store interface and raft integration

--- main.go
+++ main.go
@@ -1,5 +1,195 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/1xyz/coolbeans/store"
+	"github.com/docopt/docopt-go"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"io/ioutil"
+	"os"
+	"time"
+)
 
+func init() {
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+}
+
+func main() {
+	usage := `coolbeans.
+
+Usage:
+  coolbeans [--node-id=<id>]  [--config-file=<file>] [--join-id=<id>] 
+  coolbeans -h | --help
+
+Options:
+  -h --help                 Show this screen.
+  --node-id=<id>            unique identifier for this node.
+  --join-id=<id>            unique identifier of the node to join with the cluster [default: ].
+  --config-file=<config>  	configuration json file [default: cb-config.json].`
+
+	opts, err := docopt.ParseDoc(usage)
+	if err != nil {
+		log.Fatalf("error parsing arguments. err=%v", err)
+	}
+
+	fmt.Printf("opts=%v\n", opts)
+
+	localNodeID, err := opts.String("--node-id")
+	if err != nil {
+		log.Fatalf("error reading --node-id %v", err)
+	}
+
+	joinNodeID, err := opts.String("--join-id")
+	if err != nil {
+		log.Fatalf("error reading --join-id %v", err)
+	}
+
+	file, err := opts.String("--config-file")
+	if err != nil {
+		log.Fatalf("error reading --config-file variable %v", err)
+	}
+
+	c, err := LoadFrom(file)
+	if err != nil {
+		log.Fatalf("cannot load config from file=%v err=%v", file, err)
+	}
+
+	log.Infof("NodeID = %v config-%v JoinNodeID=%v", localNodeID, c, joinNodeID)
+
+	if err := runCoolbeans(c, localNodeID, joinNodeID); err != nil {
+		log.Fatalf("runcoolbeans err = %v", err)
+	}
+}
+
+type Config struct {
+	// Cluster wide configuration
+	Cluster struct {
+		Name                string   `json:"name"`
+		InMem               bool     `json:"in_mem"`
+		RaftTimeout         Duration `json:"raft_timeout"`
+		RestoreTimeout      Duration `json:"restore_timeout"`
+		RetainSnasphotCount int      `json:"retain_snasphot_count"`
+		MaxPool             int      `json:"max_pool"`
+	} `json:"cluster"`
+	// Node(s) specific configuration
+	Nodes []NodeConfig `json:"nodes"`
+}
+
+type NodeConfig struct {
+	ID         string `json:"id"`
+	ListenAddr string `json:"listen_addr"`
+	RaftAddr   string `json:"raft_addr"`
+	RootDir    string `json:"root_dir"`
+}
+
+func (c *Config) getNode(nodeID string) (*NodeConfig, error) {
+	for _, nc := range c.Nodes {
+		if nc.ID == nodeID {
+			return &nc, nil
+		}
+	}
+
+	return nil, fmt.Errorf("node with id=%v not found", nodeID)
+}
+
+type Duration time.Duration
+
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Duration(d).String())
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	switch value := v.(type) {
+	case float64:
+		*d = Duration(time.Duration(value))
+		return nil
+	case string:
+		tmp, err := time.ParseDuration(value)
+		if err != nil {
+			return err
+		}
+		*d = Duration(tmp)
+		return nil
+	default:
+		return fmt.Errorf("invalid duration")
+	}
+}
+
+func (d Duration) String() string {
+	return fmt.Sprintf("%v", time.Duration(d))
+}
+
+func ReadFrom(r io.Reader) (*Config, error) {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	var c Config
+	if err := json.Unmarshal(b, &c); err != nil {
+		return nil, err
+	} else {
+		return &c, nil
+	}
+}
+
+func LoadFrom(filename string) (*Config, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Errorf("error closing file %v", err)
+		}
+	}()
+	return ReadFrom(f)
+}
+
+func runCoolbeans(c *Config, nodeID string, joinID string) error {
+	logc := log.WithField("method", "runCoolBeans")
+	nc, err := c.getNode(nodeID)
+	if err != nil {
+		return err
+	}
+
+	logc.Infof("creating directory for node=%v at %v", nc.ID, nc.RootDir)
+	if err := os.MkdirAll(nc.RootDir, 0700); err != nil {
+		return err
+	}
+
+	s, err := store.NewStore(&store.Config{
+		RetainSnasphotCount: c.Cluster.RetainSnasphotCount,
+		MaxPool:             c.Cluster.MaxPool,
+		RaftTimeout:         time.Duration(c.Cluster.RaftTimeout),
+		RestoreTimeout:      time.Duration(c.Cluster.RestoreTimeout),
+		RootDir:             nc.RootDir,
+		RaftBindAddr:        nc.RaftAddr,
+		Inmem:               c.Cluster.InMem,
+	})
+	if err != nil {
+		return err
+	}
+
+	enableSingle := false
+	if len(c.Nodes) == 1 {
+		enableSingle = true
+	}
+
+	if err := s.Open(enableSingle, nodeID); err != nil {
+		logc.Errorf("store.Open enableSingle=%v nodeId=%v err=%v",
+			enableSingle, nodeID, err)
+		return err
+	}
+
+	return nil
 }
--- store/store.go
+++ store/store.go
@@ -1,17 +1,176 @@
 package store
 
 import (
+	"fmt"
 	"github.com/1xyz/beanstalkd/state"
 	"github.com/hashicorp/raft"
+	raftboltdb "github.com/hashicorp/raft-boltdb"
+	log "github.com/sirupsen/logrus"
 	"io"
+	"net"
+	"os"
+	"path/filepath"
 	"time"
 )
 
-type store struct {
-	jsm state.JSM
+type Config struct {
+	// retainSnapshotCount indicates the max, number of snapshots to retain
+	RetainSnasphotCount int
 
-	// Timeout for a restore operation
-	restoreTimeout time.Duration
+	// The MaxPool controls how many connections we will pool. The
+	MaxPool int
+
+	// RaftTimeout is the max. duration for a raft apply op.
+	// timeout is used to apply I/O deadlines. For InstallSnapshot, we multiply
+	// the timeout by (SnapshotSize / TimeoutScale).
+	RaftTimeout time.Duration
+
+	// RestoreTimeout is the max. duration for a restore operation
+	RestoreTimeout time.Duration
+
+	// RootDir is the root directory where store data is persisted
+	RootDir string
+
+	// RaftBindAddr is the bind address for raft tcp conn.
+	RaftBindAddr string
+
+	// Inmem is a boolean, controls if the data is persisted
+	Inmem bool
+}
+
+type Store struct {
+	jsm     state.JSM
+	c       *Config
+	raft    *raft.Raft
+	localID string
+}
+
+func NewStore(c *Config) (*Store, error) {
+	jsm, err := state.NewJSM()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Store{
+		jsm:  jsm,
+		c:    c,
+		raft: nil,
+	}, nil
+}
+
+// Open opens the store. If enableSingle is set, and there are no existing peers,
+// then this node becomes the first node, and therefore leader, of the cluster.
+// localID should be the server identifier for this node.
+func (s *Store) Open(enableSingle bool, localID string) error {
+	s.localID = localID
+	// Setup Raft configuration.
+	config := raft.DefaultConfig()
+	config.LocalID = raft.ServerID(localID)
+
+	// Setup Raft communication.
+	addr, err := net.ResolveTCPAddr("tcp", s.c.RaftBindAddr)
+	if err != nil {
+		return err
+	}
+
+	transport, err := raft.NewTCPTransport(s.c.RaftBindAddr, addr,
+		s.c.MaxPool, s.c.RaftTimeout, os.Stderr)
+	if err != nil {
+		return err
+	}
+
+	// Create the snapshot store. This allows the Raft to truncate the log
+	snapshots, err := raft.NewFileSnapshotStore(s.c.RootDir, s.c.RetainSnasphotCount, os.Stderr)
+	if err != nil {
+		return fmt.Errorf("file snapshot store: %s", err)
+	}
+
+	// Create the log store and stable store.
+	var logStore raft.LogStore
+	var stableStore raft.StableStore
+	if s.c.Inmem {
+		logStore = raft.NewInmemStore()
+		stableStore = raft.NewInmemStore()
+	} else {
+		boltDB, err := raftboltdb.NewBoltStore(filepath.Join(s.c.RootDir, "raft.db"))
+		if err != nil {
+			return fmt.Errorf("new bolt store: %s", err)
+		}
+
+		logStore = boltDB
+		stableStore = boltDB
+	}
+
+	// Instantiate the Raft systems.
+	ra, err := raft.NewRaft(config, (*fsm)(s), logStore, stableStore, snapshots, transport)
+	if err != nil {
+		return fmt.Errorf("new raft: %s", err)
+	}
+
+	s.raft = ra
+	if enableSingle {
+		configuration := raft.Configuration{
+			Servers: []raft.Server{
+				{
+					ID:      config.LocalID,
+					Address: transport.LocalAddr(),
+				},
+			},
+		}
+		ra.BootstrapCluster(configuration)
+	}
+
+	return nil
+}
+
+// Join joins a node, identified by nodeID and located at addr, to this store.
+// The node must be ready to respond to Raft communications at that address.
+func (s *Store) Join(nodeID, addr string) error {
+	logc := log.WithFields(log.Fields{
+		"method":  "Join",
+		"localID": s.localID})
+
+	logc.Infof("received join request for remote nodeID %s at addr:%s", nodeID, addr)
+	configFuture := s.raft.GetConfiguration()
+	if err := configFuture.Error(); err != nil {
+		logc.Errorf("failed to get raft configuration: %v", err)
+		return err
+	}
+
+	for _, srv := range configFuture.Configuration().Servers {
+		// If a node already exists with either the joining node's ID or address,
+		// that node may need to be removed from the config first.
+		if srv.ID == raft.ServerID(nodeID) || srv.Address == raft.ServerAddress(addr) {
+			// However if *both* the ID and the address are the same, then nothing -- not even
+			// a join operation -- is needed.
+			if srv.Address == raft.ServerAddress(addr) && srv.ID == raft.ServerID(nodeID) {
+				logc.Warnf("node %s at %s already member of cluster, ignoring join request",
+					nodeID, addr)
+				return nil
+			}
+
+			future := s.raft.RemoveServer(srv.ID, 0, 0)
+			if err := future.Error(); err != nil {
+				return fmt.Errorf("error removing existing node %s at %s: %s", nodeID, addr, err)
+			}
+		}
+	}
+
+	f := s.raft.AddVoter(raft.ServerID(nodeID), raft.ServerAddress(addr), 0, 0)
+	if f.Error() != nil {
+		logc.Errorf("s.raft.AddVoter. err = %v", f.Error())
+		return f.Error()
+	}
+
+	logc.Infof("node %s at %s joined successfully", nodeID, addr)
+	return nil
+}
+
+type fsm Store
+
+// Apply applies a Raft log entry to the key-value store.
+func (f *fsm) Apply(l *raft.Log) interface{} {
+	return nil
 }
 
 // Snapshot is used to support log compaction. This call should
@@ -20,13 +179,13 @@ type store struct {
 // threads, but Apply will be called concurrently with Persist. This means
 // the FSM should be implemented in a fashion that allows for concurrent
 // updates while a snapshot is happening.
-func (s *store) Snapshot() (raft.FSMSnapshot, error) {
-	return NewSnapshotFrom(s.jsm)
+func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
+	return NewSnapshotFrom(f.jsm)
 }
 
 // Restore is used to restore an FSM from a snapshot. It is not called
 // concurrently with any other command. The FSM must discard all previous
 // state.
-func (s *store) Restore(r io.ReadCloser) error {
-	return RestoreSnapshotTo(r, s.jsm, s.restoreTimeout)
+func (f *fsm) Restore(r io.ReadCloser) error {
+	return RestoreSnapshotTo(r, f.jsm, f.c.RestoreTimeout)
 }
